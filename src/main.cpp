4ca9400fc5de06b0da0dea51c94a49a918d1e532
Lukasz <lukasz.sromek@gmail.com>
2023-02-18 15:29:02 +0100

    stationary panel

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,12 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+private:
+    wxPanel *item;
+
+    wxButton *startButton;
+    wxButton *resetButton;
 };
 
 bool MyApp::OnInit()
@@ -24,4 +30,30 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
+    auto mainSizer = new wxBoxSizer(wxVERTICAL);
+    auto animPanel = new wxPanel(this, wxID_ANY);
+    animPanel->SetBackgroundColour(wxColour(100, 100, 200));
+
+    item = new wxPanel(animPanel, wxID_ANY, wxDefaultPosition, FromDIP(wxSize(100, 100)));
+    item->SetBackgroundColour(wxColour(200, 100, 100));
+
+    auto buttonsPanel = new wxPanel(this, wxID_ANY);
+    auto buttonsSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    startButton = new wxButton(buttonsPanel, wxID_ANY, "Start");
+    resetButton = new wxButton(buttonsPanel, wxID_ANY, "Reset");
+
+    resetButton->Disable();
+
+    buttonsSizer->Add(startButton, 0, wxALL, FromDIP(10));
+    buttonsSizer->Add(resetButton, 0, wxALL, FromDIP(10));
+
+    buttonsPanel->SetSizer(buttonsSizer);
+
+    mainSizer->Add(animPanel, 1, wxEXPAND);
+    mainSizer->Add(buttonsPanel, 0, wxEXPAND);
+
+    mainSizer->SetMinSize(FromDIP(wxSize(500, 600)));
+
+    this->SetSizerAndFit(mainSizer);
 }
